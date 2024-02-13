25b98edb090f1aad8524208f27b79e136064da45
heavy-coder-saksham97 <159911059+heavy-coder-saksham97@users.noreply.github.com>
2024-02-13 18:16:15 +0530

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,1 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for (int i = 1; i <= 5; i++)
+    {  
+        if (i == 1)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                cout << j << endl;
+                ;
+            }
+        }
+        else
+        {
+            int k[i + 1];
+            for (int j = 1; j <= i + 1; j++)
+            {
+                if ((j == 1) || (j == 2))
+                {
+                    k[j] = 1;
+                    cout << k[j];
+                }
+                else
+                {
+                    k[j] = k[j - 1] + k[j - 2];
+                    cout << k[j];
+                }
+            }
+            cout << endl;
+        }
+    }
+
+    return 0;
+}
 
